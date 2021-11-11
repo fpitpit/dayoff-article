40da794f399108bc56d85f0b61c34afeb8b34266
Fabrice PITOISET <fabrice.pitoiset@publicissapient.com>
2021-11-11 12:05:46 +0000

    rebase main into develop

--- data/src/main/java/fr/pitdev/dayoff/data/di/DataModule.kt
+++ data/src/main/java/fr/pitdev/dayoff/data/di/DataModule.kt
@@ -10,7 +10,6 @@ import fr.pitdev.dayoff.data.local.datasource.LocalDataSourceImpl
 import fr.pitdev.dayoff.data.remote.api.DayOffApiService
 import fr.pitdev.dayoff.data.remote.datasource.DayOffRemoteDataSource
 import fr.pitdev.dayoff.data.remote.datasource.DayOffRemoteDataSourceImpl
-import fr.pitdev.dayoff.data.repository.DayOffRepositoryImpl
 import fr.pitdev.dayoff.data.room.entities.DayOffDao
 import fr.pitdev.dayoff.domain.repository.DayOffRepository
 import javax.inject.Singleton
@@ -18,13 +17,6 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 object DataModule {
-
-    @Provides
-    @Singleton
-    fun provideLocalDataSourceInFile(dayOffDao: DayOffDao): LocalDataSource {
-        return LocalDataSourceImpl(dayOffDao)
-    }
-
     @Provides
     @Singleton
     fun provideDayOffRepository(
@@ -35,16 +27,14 @@ object DataModule {
         return DayOffRepositoryImpl(dispatchProvider, remoteDataSource, localDataSource)
     }
 
-}
-
-@InstallIn(SingletonComponent::class)
-@Module
-object RemoteSourceModule {
     @Provides
-    @Singleton
     fun provideRemoteDataSource(apiService: DayOffApiService): DayOffRemoteDataSource {
         return DayOffRemoteDataSourceImpl(apiService)
     }
+
+    @Provides
+    fun provideLocalDataSourceInFile(dayOffDao: DayOffDao): LocalDataSource {
+        return LocalDataSourceImpl(dayOffDao)
+    }
+
 }
-
-
