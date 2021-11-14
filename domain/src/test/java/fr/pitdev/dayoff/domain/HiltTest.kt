8baf2228661fcd13b698c759eff2b50cbd8fa04a
Fabrice PITOISET <fabrice.pitoiset@publicissapient.com>
2021-11-14 11:40:01 +0000

    Feature/use case

--- domain/src/test/java/fr/pitdev/dayoff/domain/HiltTest.kt
+++ domain/src/test/java/fr/pitdev/dayoff/domain/HiltTest.kt
@@ -1,7 +1,6 @@
 package fr.pitdev.dayoff.domain
 
 import androidx.annotation.CallSuper
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.HiltTestApplication
@@ -9,7 +8,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
-import org.junit.rules.TestRule
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
@@ -19,10 +17,14 @@ import org.robolectric.annotation.LooperMode
 @Config(application = HiltTestApplication::class, sdk = [31], manifest = Config.NONE)
 @RunWith(RobolectricTestRunner::class)
 @LooperMode(LooperMode.Mode.PAUSED)
-abstract class HiltTest: BaseTest() {
+abstract class HiltTest {
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
 
 
     @ExperimentalCoroutinesApi
