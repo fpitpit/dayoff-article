0b46e245d15a4202f01bb843deb186dceb20a8f0
Fabrice PITOISET <fpitoiset.pro@gmail.com>
2021-11-20 00:46:49 +0100

    fix TU

--- presentation/src/main/java/fr/pitdev/dayoff/presentation/viewmodels/DayOffsViewModel.kt
+++ presentation/src/main/java/fr/pitdev/dayoff/presentation/viewmodels/DayOffsViewModel.kt
@@ -2,17 +2,16 @@ package fr.pitdev.dayoff.presentation.viewmodels
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import fr.pitdev.dayoff.common.coroutines.CoroutineDispatcherProvider
 import fr.pitdev.dayoff.common.utils.network.NetworkStatus
 import fr.pitdev.dayoff.domain.models.DayOff
 import fr.pitdev.dayoff.domain.models.Zone
 import fr.pitdev.dayoff.domain.usecases.dayoffs.GetDayOffsUseCase
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.launchIn
 import java.time.LocalDate
 import javax.inject.Inject
 
@@ -23,14 +22,15 @@ class DayOffsViewModel @Inject constructor(
     private val dispatchProvider: CoroutineDispatcherProvider
 ) : ViewModel() {
 
-
-    fun getDayOffs(zone: Zone = Zone.METROPOLE, year: Int = LocalDate.now().year) =
-        flow<NetworkStatus<List<DayOff>>> {
+    fun getDayOffs(
+        zone: Zone = Zone.METROPOLE,
+        year: Int = LocalDate.now().year
+    ): Flow<NetworkStatus<List<DayOff>>> =
+        flow {
             getDayOffsUseCase(zone, year).collect {
                 emit(it)
             }
-        }.flowOn(dispatchProvider.io).launchIn(viewModelScope)
-
+        }.flowOn(dispatchProvider.io)
 
 }
 
